68ae13171e648eb27e94b730b57d0ca85509f1f1
Vanessa Florio <vanessaflorio1997@gmail.com>
2022-01-14 12:18:53 +0000

    Step 11 complete - wrapping database data in program objects'

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'rspec'
 gem 'capybara'
 gem 'webrick'
 gem 'pg'
+gem 'simplecov'
+gem 'simplecov-console'
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,49 +1,36 @@
 require 'bookmark'
+require 'database_helpers'
 
 describe Bookmark do
   
   let(:url) { 'http://www.youtube.com/' }
   let(:title) { 'Test Title' }
   
-  describe '.all' do
+  describe '.get_all' do
     it 'returns all bookmarks' do
-      Bookmark.add("http://www.makersacademy.com/", 'Makers Academy')
+      bookmark = Bookmark.add("http://www.makersacademy.com/", 'Makers Academy')
       Bookmark.add("http://www.destroyallsoftware.com/", 'Destroy all software')
       Bookmark.add("http://www.google.com/", 'google')
       
-      bookmarks = Bookmark.all
+      bookmarks = Bookmark.get_all
       
-      expect(bookmarks).to include({:title => "Makers Academy", :url => "http://www.makersacademy.com/"})
-      expect(bookmarks).to include({:title => "Destroy all software", :url => "http://www.destroyallsoftware.com/"})
-      expect(bookmarks).to include({:title => "google", :url => "http://www.google.com/"})
+      expect(bookmarks.length).to eq 3
+      expect(bookmarks.first).to be_a Bookmark
+      expect(bookmarks.first.id).to eq bookmark.id
+      expect(bookmarks.first.title).to eq 'Makers Academy' 
+      expect(bookmarks.first.url).to eq 'http://www.makersacademy.com/'
     end
   end
 
   describe '.add'
     it 'adds a bookmark to the database' do
-      bookmark = Bookmark.add(url, title).first
+      bookmark = Bookmark.add(url, title)
+      persisted_data = persisted_data(bookmark.id)
       #expect(Bookmark.all).to include({title: title, url: url})
-      expect(bookmark['url']).to eq url
-      expect(bookmark['title']).to eq title
+      expect(bookmark.id).to eq persisted_data['id']
+      expect(bookmark.title).to eq 'Test Title'
+      expect(bookmark.url).to eq 'http://www.youtube.com/'
     end
+    
 end
 
-#Walkthrough Comments - Stel 11
-# describe '.all' do
-#   it 'returns a list of bookmarks' do
-#     connection = PG.connect(dbname: 'bookmark_manager_test')
- 
-#     # Add the test data
-#     bookmark = Bookmark.create(url: "http://www.makersacademy.com", title: "Makers Academy")
-#     Bookmark.create(url: "http://www.destroyallsoftware.com", title: "Destroy All Software")
-#     Bookmark.create(url: "http://www.google.com", title: "Google")
- 
-#     bookmarks = Bookmark.all
- 
-#     expect(bookmarks.length).to eq 3
-#     expect(bookmarks.first).to be_a Bookmark
-#     expect(bookmarks.first.id).to eq bookmark.id
-#     expect(bookmarks.first.title).to eq 'Makers Academy'
-#     expect(bookmarks.first.url).to eq 'http://www.makersacademy.com'
-#    end
-#  end
